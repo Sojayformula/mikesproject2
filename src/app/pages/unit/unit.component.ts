fc0fde022d4deae2b30ed75fa2070aeed51692c4
Emma So <your-email@example.com>
2025-05-30 16:44:03 +0000

    Save changes before installing ng-zorro-antd

--- src/app/pages/unit/unit.component.ts
+++ src/app/pages/unit/unit.component.ts
@@ -24,7 +24,7 @@ export class UnitComponent implements OnInit{
 
 
   APIData: any[] = [];
-  selectedTab:string = 'Active'
+  selectedTab:string = 'Active';
   unitModel!: unitModel
   editModel!: updateUnitModel
 
